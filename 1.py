3a0f32d73865eca2cb1709c967fd393bca9df294
Elena Lanskaya <77541150+ElenLan@users.noreply.github.com>
2022-04-07 20:25:23 +0500

    Add files via upload

--- 1.py
+++ 1.py
@@ -14,29 +14,74 @@ import requests
 from bs4 import BeautifulSoup as bs
 from fake_headers import Headers
 from pprint import pprint
+import time
+import random
+import json
 
 # https://hh.ru/search/vacancy?search_field=name&search_field=company_name&search_field=description&text=
 # Python+developer&from=suggest_post&customDomain=1
-
 base_url = 'https://hh.ru'
-url = base_url + '/search/vacancy?text=python+developer&clusters=true&ored_clusters=true&enable_snippets=true'
+url = base_url + '/search/vacancy?text=&page='
+params = {'text': str(input('введите желаемую доллжность: ')),
+          'page': 0}
 header = Headers(headers=True).generate()
-response = requests.get(url, headers=header)
-# with open ('response.html', 'w', encoding='utf-8') as f:
-#     f.write(response.text)
-dom = bs(response.text, 'html.parser')
-vacancies = dom.find_all('div', {'class': 'vacancy-serp-item'})
-# vacancy_salary = dom.find('span', {'class': 'bloko-header-section-3'}).text
-# pprint(vacancy_salary) -- Здесь возвращает результат методом find  одном экземпляре
-vacancy_list = []
-for vacancy in vacancies:
-    vacancy_data = {}
-    vacancy_name = vacancy.find('a', {'class': 'bloko-link'}).getText()
-    vacancy_salary = vacancy.find('span', {'class': 'bloko-header-section-3'}).getText() # метод text и getText возвращают ошибку
-    # pprint(vacancy_salary)
-    vacancy_link = vacancy.find('a', {'class': 'bloko-link'})['href']
-    vacancy_data['name'] = vacancy_name
-    vacancy_data['salary'] = vacancy_salary
-    vacancy_data['link'] = vacancy_link
-    vacancy_list.append(vacancy_data)
-pprint(vacancy_list)
+pages = 40
+n = 1
+while params['page'] <= pages:
+    response = requests.get(url, headers=header, params=params)
+    # with open('response.html', 'w', encoding='utf-8') as f:
+    #     f.write(response.text)
+    # html_file = ''
+    # with open('response.html', 'r', encoding='utf-8') as f:
+    #     html = f.read()
+    dom = bs(response.text, 'html.parser')
+    vacancies = dom.find_all('div', {'class': 'vacancy-serp-item'})
+    vacancy_list = []
+    for vacancy in vacancies:
+        vacancy_data = {}
+        vacancy_name = vacancy.find('a', {'class': 'bloko-link'}).getText()
+        vacancy_salary = str(vacancy.find('span', {'class': 'bloko-header-section-3'})).removeprefix(
+            '<span class="bloko'
+            '-header-section-3" '
+            'data-qa="vacancy'
+            '-serp__vacancy'
+            '-compensation">'). \
+            removesuffix('</span>').replace('<!-- --', '').replace('>', '').replace('\u202f', '').split(' ')
+        salary_list = []
+        salary_data = {}
+        if 'None' in vacancy_salary:
+            s_min = None
+            s_max = None
+            currency = None
+        elif 'до' in vacancy_salary:
+            s_min = None
+            s_max = vacancy_salary[1]
+            currency = vacancy_salary[-1]
+        elif 'от' in vacancy_salary:
+            s_min = vacancy_salary[1]
+            s_max = None
+            currency = vacancy_salary[-1]
+        elif '-' in vacancy_salary:
+            s_min = vacancy_salary[0]
+            s_max = vacancy_salary[2]
+            currency = vacancy_salary[-1]
+        salary_data['min'] = s_min
+        salary_data['max'] = s_max
+        salary_data['currency'] = currency
+        salary_list.append(salary_data)
+        vacancy_link = vacancy.find('a', {'class': 'bloko-link'})['href']
+        vacancy_data['name'] = vacancy_name
+        vacancy_data['salary'] = salary_list
+        vacancy_data['link'] = vacancy_link
+        vacancy_list.append(vacancy_data)
+        time.sleep(random.randint(1, 10))
+        with open('vacancies.json', 'w', encoding='UTF-8') as f:
+            json.dump(vacancy_list, f)
+    button = int(dom.find_all('a', {'class': 'bloko-button'})[-2].text)
+    pages = (button if pages < button else pages)
+    params['page'] += 1
+
+    pprint(vacancy_list)
+    pprint(len(vacancy_list))
+
+# python developer
